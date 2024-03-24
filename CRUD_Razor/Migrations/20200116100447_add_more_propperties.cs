4a80814dcc82d822cc57c90c7df4172431c3e08e
javanmardi <javanmardi.ej@gmail.com>
2024-03-24 12:39:43 +0330

    feat: initialize the project

---
+++ CRUD_Razor/Migrations/20200116100447_add_more_propperties.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CRUD_Razor.Migrations
+{
+    public partial class add_more_propperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Books",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Books",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ISBN",
+                table: "Books",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "ISBN",
+                table: "Books");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Books",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
---
+++ CRUD_Razor/Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRUD_Razor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CRUD_Razor.Pages.BookList
+{
+    public class EditModel : PageModel
+    {
+
+        private readonly ApplicationDbContext _db;
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [BindProperty]
+        public Book Book { get; set; }
+        [TempData]
+        public string Massage { get; set; }
+        public void OnGet(int id)
+        {
+            Book = _db.Books.Find(id);
+        }
+
+        public async Task<IActionResult> OnPost(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                var bookFromDb = _db.Books.Find(id);
+                bookFromDb.Author = Book.Author;
+                bookFromDb.ISBN = Book.ISBN;
+                bookFromDb.Name = Book.Name;
+
+                await _db.SaveChangesAsync();
+                Massage = "Book successfully updated.";
+
+                return RedirectToPage("Index");
+            }
+
+            return RedirectToPage();
+        }
+    }
+}
